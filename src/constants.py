13cae3cb3a59dffa28db6ac04c091423285ee4d4
aazamatov <Abilay5787@gmail.com>
2023-09-07 22:59:40 +0600

    add library path

--- src/constants.py
+++ src/constants.py
@@ -1,14 +1,14 @@
-lib_path = '/home/parqour/Documents/Jobs/wagon_recognition/DTKWNRSDK_Trial/lib/linux/x86_64/'
+lib_path = '/home/parqour/DTKWNRSDK/lib/linux/x86_64/'
 DTKWNRLib = 'libDTKWNR.so'
 DTKVIDLib = 'libDTKVID.so'
 
-video_path = '/home/parqour/Documents/Jobs/wagon_recognition/DTKWNRSDK_Trial/video/sample.mp4'
+video_path = '/home/parqour/DTKWNRSDK/video/wagon_test_video_2.mp4'
 
-wn_count = 5
+wn_count = 30
 
 difference_coordinate = 10
 
-rec_threshold = 90
+rec_threshold = 80
 
 wns_coordinates_threshold = 100
 
