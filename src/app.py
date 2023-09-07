d350b4cb4f3a7a5a22eceadb23cdb0d0adbb5f0e
aazamatov <Abilay5787@gmail.com>
2023-09-07 23:07:40 +0600

    refactoring

--- src/app.py
+++ src/app.py
@@ -26,10 +26,10 @@ class Application(object):
             'PATH'] = constants.lib_path + os.pathsep + os.environ['PATH']
 
         # For camera
-        #self.video_path = 'rtsp://admin:campas123.@{}/media/video1'.format(camera_ip)
+        self.video_path = 'rtsp://admin:campas123.@{}/media/video0'.format(camera_ip)
 
         # For video
-        self.video_path = constants.video_path
+        #self.video_path = constants.video_path
 
         self.DTKWNR = cdll.LoadLibrary(constants.DTKWNRLib)
         self.DTKVID = cdll.LoadLibrary(constants.DTKVIDLib)
